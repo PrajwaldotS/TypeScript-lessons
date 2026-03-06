516c2e5c4772ba78debba2a701ab9331816c6a7f
prajwaldots <prajwal.s2294@gmail.com>
2026-03-06 18:28:48 +0530

    Union Types

---
+++ Lesson3/script.ts
@@ -0,0 +1,4 @@
+let a : number | string ; // Union type | this means it can be either a number or a string
+a = 10; // valid
+a = "Hello"; // valid
+// a = true; // invalid, boolean is not assignable to number or string
