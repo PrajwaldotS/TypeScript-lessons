98d78f8d09e2d82bf59d9b0594b3e7c177a720c8
prajwaldots <prajwal.s2294@gmail.com>
2026-03-06 18:35:55 +0530

    intersection types

--- Lesson3/script.ts
+++ Lesson3/script.ts
@@ -2,3 +2,19 @@ let a : number | string ; // Union type | this means it can be either a number o
 a = 10; // valid
 a = "Hello"; // valid
 // a = true; // invalid, boolean is not assignable to number or string
+
+
+type city = {
+    name: string;
+    population: number;
+}
+type planet = {
+    name:string;
+    cities:number;
+}
+type location = city & planet; // Intersection type for location that is both a city and a planet
+let value : location = {
+    name:"Earth",
+    population: 100,
+    cities: 5
+} // valid, value has properties of both city and planet
