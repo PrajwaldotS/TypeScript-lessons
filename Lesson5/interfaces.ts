10a279c384d0f1c2478cad2e5383fd8c95ab9645
prajwaldots <prajwal.s2294@gmail.com>
2026-03-06 19:10:58 +0530

    extending example of interface

--- Lesson5/interfaces.ts
+++ Lesson5/interfaces.ts
@@ -13,3 +13,16 @@ function nameHuman (human:human) {
     return human.name;
     return human.age; // so we can access both name and age properties of human interface because we have defined them in the same interface
 }
+
+// example of extending an interface
+
+interface Student extends human{
+    grade:number;
+    SchoolName:string;
+}
+function nameStudent(student:Student){
+    console.log(student.name);
+    console.log(student.age);
+    console.log(student.grade);
+    console.log(student.SchoolName);
+} // so we can access all the properties of human interface in student interface because we have extended human interface in student interface
